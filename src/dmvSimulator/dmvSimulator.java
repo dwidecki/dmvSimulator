88c42c8d1166fca6b2bbd43f476a2f03dd0ac794
dwidecki <dylan.widecki@quinnipiac.edu>
2022-03-05 12:39:30 -0500

    wait time increased to 200)

--- src/dmvSimulator/dmvSimulator.java
+++ src/dmvSimulator/dmvSimulator.java
@@ -4,14 +4,14 @@ public class dmvSimulator {
 	public static void main(String[] args) {
 		
 		System.out.println("Welcome to the DMV");
-		int number = 1 + (int) (Math.random() * 100);
+		int number = 1 + (int) (Math.random() * 200);
 		System.out.println("Your number is " + number);
 			
 		int count = number +1;
 		
 			while (count != number) {
 	
-				if(count <= 100) {
+				if(count <= 200) {
 					
 					System.out.println("The current number called is " + count);
 					count++;
